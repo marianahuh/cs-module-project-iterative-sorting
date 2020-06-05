9d4794d6ae216735804b130e0ec7c3139c4bf92e
Mariana Huh <mhuh0176@gmail.com>
2020-06-05 13:48:05 -0700

    added comments

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -10,8 +10,8 @@ def linear_search(arr, target):
     return -1   # not found
 
 
-# print('Linear Search target is at index: ', linear_search(
-#     ['a', 'b', 'c', 'd', 'e', 'f', 'g'], 'f'))
+print('Linear Search target is at index: ', linear_search(
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g'], 'f'))
 
 # Write an iterative implementation of Binary Search
 
@@ -19,23 +19,32 @@ def linear_search(arr, target):
 def binary_search(arr, target):
     low = 0
     high = len(arr) - 1
+    # as long as low is less than high
     while low <= high:
+        # get middle position
         mid = (low + high) // 2
+        # get middle value
         mid_val = arr[mid]
         print('\nBinary Search')
         print('TARGET:', target)
         print(arr)
         print('low:', arr[low], ' mid:', arr[mid], ' high:', arr[high])
+        # if target is found
         if mid_val == target:
             print('Target:', target, 'at position:', mid)
+            # return position of the value
             return mid
+        # if the middle value is less than target, go left
         elif mid_val > target:
+            # change high position to the value to the left of middle val
             high = mid - 1
+        # otherwise if middle value is greater, go right
         else:
+            # change low position to the value to the right of middle val
             low = mid + 1
 
     return -1  # not found
 
 
-# print('Binary Search target is at index: ', binary_search(
-#     [11, 22, 33, 44, 55, 66, 77, 88, 99], 33))
+print('Binary Search target is at index: ', binary_search(
+    [11, 22, 33, 44, 55, 66, 77, 88, 99], 33))
